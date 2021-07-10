c30c010d4873cbb39390c293134a4826bd68303f
matthewstuartrowe2 <78889074+matthewstuartrowe2@users.noreply.github.com>
2021-07-10 13:51:48 -0400

    Make both die pick random number when button is pushed
    Finish app
    Simple Sample App

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -15,30 +15,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     //Right Dice
     @IBOutlet weak var diceImageView2: UIImageView!
-    
-    var leftDiceNumber = 0//the position in the dice array located below, default left dice's number in array (aka dice two)
-    
-    var rightDiceNumber = 5//the position in the dice array located below, default right dice's number in array (aka dice 6)
-    
-    override func viewDidLoad() {
-        //This is what shows as soon as the app launches.
-        super.viewDidLoad()
-        //Who.What = Where
-        //example diceImageView1.image = imageliteral
-        
-    }
-    
     //IBAction area is for the UI to tell the code what to do
     //Add the button here use the control button and tap the button within the UI
     //Then name the button, select it as UIButton, and select Touch Up Inside to click the button within the button box for it to work.
     //This is the roll button on the UI
     //This is called an IBAction not an outlet, its an action which happens when you click the thing
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        //print("Button got tapped.")//Prints this statement into the console.
-        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][leftDiceNumber]//the second [] is the position within the array, this is referenced by the leftDiceNumber variable above
-        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][rightDiceNumber]//the second [] is the position within the array, this is referenced by the leftDiceNumber variable above
-        leftDiceNumber = leftDiceNumber + 1
-        rightDiceNumber = rightDiceNumber - 1
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        //Create a random number between 0 and 5 by typing Int.random(in:0...5) OR you can instead use .randomElement() which takes a random element in the array and there is no need to specify the range of the random numbers. USED FOR ARRAYS.
+        diceImageView1.image = diceArray.randomElement()//the second [] is the position within the array, this will be a random location in the array between 0 and 5
+        
+        diceImageView2.image = diceArray.randomElement()
+
+        
+        
     }
     
 }
